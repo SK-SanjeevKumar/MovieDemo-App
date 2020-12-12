993fc7e67fbefe7af6ea05212fb2f4a40a4ef26d
SK-SanjeevKumar <57248953+SK-SanjeevKumar@users.noreply.github.com>
2020-12-13 00:30:12 +0530

    Add files via upload

---
+++ build/precache-manifest.930aa2cca209da091093f71ab3400763.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b24035c03594b6f46515c24f753db472",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53a367d6340f562751f6",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d7b5298a6b67f37690ec",
+    "url": "/static/css/main.0cb70629.chunk.css"
+  },
+  {
+    "revision": "53a367d6340f562751f6",
+    "url": "/static/js/2.ab8669f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab8669f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b5298a6b67f37690ec",
+    "url": "/static/js/main.ec3f1235.chunk.js"
+  },
+  {
+    "revision": "bfe5595c98fa4c23c9fb",
+    "url": "/static/js/runtime-main.1a33624d.js"
+  }
+]);
